c49e96bcf670d3b482169732c7c9a8ca56ec2b95
Squary <29478866+squarylium@users.noreply.github.com>
2018-09-12 02:25:45 +0900

    fix luminous

--- src/luminous-custom.js
+++ src/luminous-custom.js
@@ -7,11 +7,28 @@
 
 "use strict";
 
-window.addEventListener('load', function() {
+window.addEventListener("load", function() {
   var luminousOpts = {
-    caption: function (trigger) {
-      return trigger.querySelector('img').getAttribute('alt');
+    onOpen: function() {
+      var eleBody = document.getElementsByTagName("body")[0];
+      var eleNav = document.getElementsByTagName("nav")[0];
+      var scrollbar_width = window.innerWidth - document.body.scrollWidth;
+      eleBody.style.overflow = "hidden";
+      if(scrollbar_width) {
+        eleBody.style.paddingRight = scrollbar_width + "px";
+        eleNav.style.paddingRight = scrollbar_width + "px";
+      }
+    },
+    onClose: function() {
+      var eleBody = document.getElementsByTagName("body")[0];
+      var eleNav = document.getElementsByTagName("nav")[0];
+      eleBody.style.overflow = "visible";
+      eleBody.style.paddingRight = "0";
+      eleNav.style.paddingRight = "0";
+    },
+    caption: function(trigger) {
+      return trigger.querySelector("img").getAttribute("alt");
     }
   };
-  new LuminousGallery(document.querySelectorAll('.luminous'), {}, luminousOpts);
+  new LuminousGallery(document.querySelectorAll(".luminous"), {}, luminousOpts);
 });
